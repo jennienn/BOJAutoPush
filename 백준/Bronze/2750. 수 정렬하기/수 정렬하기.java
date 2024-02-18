9c22b9747b46a4e6e56500df3e53b86d5353b1c2
YEJIN KIM <118978246+jennienn@users.noreply.github.com>
2024-02-18 18:38:29 +0900

    [Bronze II] Title: 수 정렬하기, Time: 360 ms, Memory: 22340 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = a[minIndex];
+            a[minIndex] = a[i];
+            a[i] = temp;
+        }
+
+        for (int num : a) {
+            System.out.println(num);
+        }
+    }
+}
